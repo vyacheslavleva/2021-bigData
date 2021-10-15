c3d8f59130385eeb57008d2afc0aa0d57f58ed27
beresnev <beresven@gmail.com>
2021-10-15 13:36:54 +0300

    Практика1

--- Береснев Даниил Романович/1.py
+++ Береснев Даниил Романович/1.py
@@ -1,5 +1,26 @@
 import numpy as np
+
 import pandas as pd
 
-data = pd.util.testing.makeDataFrame()
-data.head()
+rng = np.random.default_rng()
+
+df1 = pd.DataFrame(rng.integers(-1000,5000,size = (9000,4)))
+df2 = pd.DataFrame(rng.random((9000,3)))
+df = pd.concat([df1,df2],axis = 1)
+
+df.to_csv('beresnev_1', sep='\t',index=False)
+
+df = pd.read_csv('beresnev_1', sep='\t')
+
+df.columns = [x for x in range(0,7)]
+
+#print(df.head())
+
+for i in range(150):
+    df.at[np.random.randint(0,9000),0] = 0
+    df.at[np.random.randint(0,9000),3] = None
+
+#print(df.head())
+
+df.to_csv('beresnev_2', sep='\t', index=False)
+
