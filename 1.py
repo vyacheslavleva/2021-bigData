e27fe44a4ad33e1f3478d645d0fabf9d00cbd0e2
kit8nino <freezer.nik@gmail.com>
2021-10-15 12:56:29 +0300

    Пример замены некоторых целочисленных
    В качестве отправной точки. Вам надо еще и часть  float данных попортить

--- 1.py
+++ 1.py
@@ -3,13 +3,19 @@ import pandas as pd
 
 rng = np.random.default_rng()
 df1 = pd.DataFrame(rng.integers(-1000, 5000, size=(9000, 4)))
-df2 = pd.DataFrame(rng.random((3000, 3))) 
-        
+df2 = pd.DataFrame(rng.random(size=(9000, 3)))
 
 data = pd.concat([df1, df2], axis=1)
 
+data.columns = [x for x in range(7)]
+
 data.to_csv('morozov', sep='\t')
 
-df = pd.read_csv('morozov', sep='\t')
+df = pd.read_csv('morozov_1', sep='\t')
 
-print(df.head())
+print(df)
+
+for i in range(100):
+    df.at[2, np.random.randint(0, 9000)] = None
+    df.at[1, np.random.randint(0, 9000)] = -2
+    df.at[0, np.random.randint(0, 9000)] = rng.random()
