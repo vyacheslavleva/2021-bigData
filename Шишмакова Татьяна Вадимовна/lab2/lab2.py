7f1390d4d7e683587d314e59d129336768ed0083
Татьяна <92569028+Shishmakova@users.noreply.github.com>
2021-12-03 15:27:21 +0300

    [2] Лабораторная работа 2
    Добавила второй график

--- Шишмакова Татьяна Вадимовна/lab2/lab2.py
+++ Шишмакова Татьяна Вадимовна/lab2/lab2.py
@@ -18,7 +18,7 @@ df4 = df3.groupby(['king', 'year'], dropna=True)[['count']]
 df4.agg('sum').to_csv('result', sep='\t')
 df4 = pd.read_csv('result', header=0, sep='\t', na_filter=True, names=['king', 'year', 'count'])
 print(df4)
-
+print("")
 
 x = df4['year']
 y = df4['king']
@@ -26,11 +26,33 @@ c=df4['count']
 fig, ax = plt.subplots()
 plt.scatter(x, y,c*12) 
 plt.xlabel('Year')
+plt.title('Абсолютные значения')
 
 for x1, y1, s1 in zip(x, y, c):
-    label = s1
-    plt.annotate(s1,
-                (x1, y1),
+    plt.annotate(s1,(x1, y1),
+                 textcoords="offset points",
+                 xytext=(6, -4),
+                 ha='left',fontsize=14)
+
+ax.set(xlim=(297, 301), xticks=np.arange(298, 301))
+plt.show()
+print("")
+
+df5=len(df4['count'])
+df6=df4['count']
+i=0
+summa=0
+while i<df5:
+  summa=summa+df6[i]
+  i=i+1
+
+fig, ax = plt.subplots()
+plt.scatter(x, y,c*12) 
+plt.xlabel('Year')
+plt.title('Нормированные значения (%)')
+
+for x1, y1, s1 in zip(x, y, round(c/summa*100,2)):
+    plt.annotate(s1,(x1, y1),
                  textcoords="offset points",
                  xytext=(6, -4),
                  ha='left',fontsize=14)
