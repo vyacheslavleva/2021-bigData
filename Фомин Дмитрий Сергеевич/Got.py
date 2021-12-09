3e21c6fddb007ff47f5100dead7a742f5c79ad4a
Dimasaurus <92543865+Dimasaurus@users.noreply.github.com>
2021-12-09 20:38:48 +0300

    lab 2

--- Фомин Дмитрий Сергеевич/Got.py
+++ Фомин Дмитрий Сергеевич/Got.py
@@ -1,16 +1,24 @@
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import pandas as pd
 import numpy as np
 
 data = pd.read_csv('../_lab-2/GoT/battles.csv',header=0)
 print(data.head())
-def func(data):
-    data.loc[data['defender_1'] == 'Tully'].groupby(['year'])['battle_number'].nunique()
-    
-    return void
 
-data.map(func,data)
-data.plot(kind = 'bar')
+pd.options.display.max_columns = 20
+
+defenders = data['defender_1'].dropna().unique()    
+print(defenders)
+dataCollector=pd.DataFrame(data=None)
+
+for i in defenders:
+    y=data.loc[data['defender_1'] == i].groupby('year')['battle_number'].nunique(dropna=True)
+    dataCollector=pd.concat([dataCollector,y],axis=1)
+dataCollector.columns = defenders
+graph = dataCollector.plot(kind = 'bar',stacked=True, colormap='Paired')
+for i in range(data['defender_1'].dropna().nunique()):           
+    graph.bar_label(graph.containers[i],label_type='center')
 
 plt.show()
 
