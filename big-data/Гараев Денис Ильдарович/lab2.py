a4d08dfed8be8f41b5ad57c7ebfd57ae8a8384b9
kit8nino <freezer.nik@gmail.com>
2022-02-18 11:04:40 +0300

    [-] второй семестр

--- big-data/Гараев Денис Ильдарович/lab2.py
+++ big-data/Гараев Денис Ильдарович/lab2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:big-data/Гараев Денис Ильдарович/lab2.py
 from os import name
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -44,50 +43,3 @@ ax.legend()
 fig.tight_layout()
 
 plt.show()
-=======
-from os import name
-import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-import numpy as np
-from pandas.core.indexes.multi import MultiIndex
-
-data = pd.read_csv(
-    './_lab-2/GoT/battles.csv', header=0)
-
-# print( data.groupby(['year', 'defender_1'])['name'].nunique(dropna=True) )
-
-fightsPerYear = data.groupby(['year', 'defender_1'])[
-    'name'].nunique(dropna=True)
-defenders = pd.Series(data['defender_1'].sort_values(
-    ascending=True).dropna().unique(), name='defender_1')
-years = pd.Series(data['year'].dropna().unique(), name='year')
-defyear = pd.merge(years, defenders, how='cross').sort_values(by='year')
-defyeartuple = [tuple(x) for x in defyear.to_numpy()]
-defendersSeries = pd.Series(0, MultiIndex.from_tuples(
-    defyeartuple, names=['year', 'defender_1']), name='defendersSeries')
-defendersSeries = defendersSeries.combine(
-    fightsPerYear, (lambda x1, x2: x2 if x2 > x1 else x1))
-
-#print(defendersSeries)
-
-fig, ax = plt.subplots()
-
-x = np.arange(defenders.size)
-width = 0.15
-
-
-rects = []
-for i in range(0, years.size):
-    rects.append(
-        ax.bar(x + width * i , defendersSeries[years[i]], width, label=years[i]))
-    ax.bar_label(rects[i], padding=3)
-
-ax.set_ylabel('Fights')
-ax.set_xticks(x, defenders)
-ax.legend()
-
-fig.tight_layout()
-
-plt.show()
->>>>>>> b3a74a08d940566d54ff7341f69753f08fb4a851:Гараев Денис Ильдарович/lab2.py
--- big-data/Суслов Илья Дмитриевич/1.py
+++ big-data/Суслов Илья Дмитриевич/1.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-from math import factorial
 
 rng=np.random.default_rng()
 df1=pd.DataFrame(rng.integers(-1000, 5000, size=(9000,4)))
@@ -13,37 +12,5 @@ for i in range(100):
 	df.at[np.random.randint(0,9000), 0] /= 2
 	df.at[np.random.randint(0,9000), 1] = None
 	df.at[np.random.randint(0,9000), 5] = -9999999
-
-
-def process_data(df):
-    medium_value= sum(df[4]+df[5]+df[6])/(len(df[4])+len(df[5])+len(df[6]))
-    print('Medium 4:6: ', medium_value)
-    min_int_value = (sum(df[0]) + sum(df[1]) + sum(df[2]) + sum(df[3])) % 100
-    print(min_int_value)
-    print('Factorial of min % 100 of 0:4: ', factorial(min_int_value))
-
-
-def map_data(df):
-
-
-    df_int = df[df.isin(range(-1000, 5000))]
-    df_int.fillna(value=0, inplace=True)
-    df_int.drop(df_int.iloc[:, 4:7], axis=1, inplace=True)
-    df_int = df_int.convert_dtypes()
-
-    df_float = df[(df<1.)&(df>-1.)]
-    df_float.fillna(value=0, inplace=True)
-    df_float.drop(df_float.iloc[:, :4], axis=1, inplace=True)
-
-    df_filtered = pd.concat([df_int, df_float], axis=1)
-
-    print('filtered /---------------/')
-    print(df_filtered)
-
-    return df_filtered
-process_data(map_data(df))
-
 df.to_csv('suslov',sep='\t')
 print (df.head())
-
-
